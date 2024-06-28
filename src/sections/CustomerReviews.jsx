1c0fbe0ed94c1086999b383d62c7c7729aa0f0b4
Taj56 <64923628+Taj56@users.noreply.github.com>
2024-06-28 14:38:22 -0500

    customer reviews & sign up section

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,7 +1,20 @@
+import { reviews } from "../constants";
+import ReviewCard from "../components/ReviewCard";
+
 const CustomerReviews = () => {
   return (
-    <div>customerReviews</div>
+    <section className="max-container ">
+      <h3 className="font-palanquin font-bold text-4xl text-center">What Our <span className="text-coral-red ">Customers</span> Say?</h3>
+      <p className="info-text m-auto mt-4 max-w-lg text-center">Here stories from our satisfied customers</p>
+
+    <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+      {reviews.map((review) => (
+        <ReviewCard key={review.customerName} imgURL={review.imgURL} customerName={review.customerName} rating={review.rating} feedback={review.feedback} />
+      ))}
+    </div>
+
+    </section>
   )
 }
 
-export default CustomerReviews
+export default CustomerReviews;
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,7 +1,17 @@
+
+
 const Footer = () => {
   return (
-    <div>footer</div>
+    <footer className="max-container">
+      <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col">
+        <div className="flex flex-col items-start">
+          <a href="/">
+            <img src={footerLogo} alt="" />
+          </a>
+        </div>
+      </div>
+    </footer>
   )
 }
 
-export default Footer
+export default Footer;
